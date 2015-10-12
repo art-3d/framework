6d7f601a0a59ce6418a0b8cc7dec4784422950c6
Megapika4 <kaktyc3d@mail.ru>
2015-10-12 21:18:20 +0300

    PHPDoc

--- framework/Request/Request.php
+++ framework/Request/Request.php
@@ -11,30 +11,59 @@ class Request
 		//...
 	}
 	
+	/**
+	 * @return string.
+	 */
 	public function getHost(){
 		return $_SERVER['HTTP_HOST'];
-	}
+	}	
 	
+	/**
+	 * @return string.
+	 */	
 	public function getURI(){
 		return $_SERVER['REQUEST_URI'];
 	}
-	
+
+	/**
+	 * @return string.
+	 */		
 	public function getMethod(){
 		return $_SERVER['REQUEST_METHOD'];
 	}
-	
+
+	/**
+	 * @return boolean.
+	 */		
 	public function isPost(){
 		return $this->getMethod($var) == 'POST';		
 	}
-	
+
+	/**
+	 * Getter, from $_POST.
+	 * @param string.
+	 * @param string.
+	 * @return string.
+	 */		
 	public function post($varname, $filterType = 'def'){
 		return isset($_POST[$varname]) ? $this->filter($_POST[$varname], $filterType) : null;
 	}
 	
+	/**
+	 * Getter, from $_GET.
+	 * @param string.
+	 * @param string.
+	 * @return string.
+	 */
 	public function get($varname, $filterType = 'def'){
 		return isset($_GET[$varname]) ? $this->filter($_GET[$varname], $filterType) : null;
 	}
 	
+	/**
+	 * @param string.
+	 * @param string.
+	 * @return string.
+	 */
 	private function filter($var, $filterType = 'def'){
 		
 		switch($filterType){
